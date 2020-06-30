30debe5a8da8367d6876293e6205a8a325b61891
Deddy Romnan <romnandeddy@gmail.com>
2020-06-30 18:11:35 +0700

    Nyoba pake loop audio yang lebih bagus

--- src/main/java/com/romnan/chillax/MainActivity.java
+++ src/main/java/com/romnan/chillax/MainActivity.java
@@ -1,13 +1,10 @@
 package com.romnan.chillax;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,17 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.raining_sound);
+//        final MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.raining_sound);
+
         final ImageView rainBtn = findViewById(R.id.rainBtn);
+//        rainBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (!mediaPlayer.isPlaying()) {
+//                    Snackbar.make(rainBtn, "Gentle rain sound started...",
+//                            Snackbar.LENGTH_LONG).show();
+//                    mediaPlayer.start();
+//                    mediaPlayer.setLooping(true);
+//                } else {
+//                    Snackbar.make(rainBtn, "Gentle rain stopped...",
+//                            Snackbar.LENGTH_LONG).show();
+//                    mediaPlayer.pause();
+//                }
+//            }
+//        });
+
         rainBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(rainBtn, "Starting rain sound", Snackbar.LENGTH_LONG).show();
-                if (!mediaPlayer.isPlaying()) {
-                    mediaPlayer.start();
-                } else {
-                    mediaPlayer.pause();
-                }
+                LoopMediaPlayer.create(MainActivity.this, R.raw.raining_sound);
             }
         });
     }
