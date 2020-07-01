899c120bed96e2ddf1dba4b689c226846d70fa0a
Deddy Romnan <romnandeddy@gmail.com>
2020-07-01 22:41:23 +0700

    - update Media player for gapless looping - update layout (bonfire button added)

--- src/main/java/com/romnan/chillax/MainActivity.java
+++ src/main/java/com/romnan/chillax/MainActivity.java
@@ -1,42 +1,58 @@
 package com.romnan.chillax;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mCurrentPlayer = new MediaPlayer(), mNextPlayer;
+    private int mResId;
+    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            mp.release();
+            mCurrentPlayer = mNextPlayer;
+            createNextMediaPlayer();
+            Log.d("MediaPlayer", "Looped");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        final MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.raining_sound);
-
         final ImageView rainBtn = findViewById(R.id.rainBtn);
-//        rainBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (!mediaPlayer.isPlaying()) {
-//                    Snackbar.make(rainBtn, "Gentle rain sound started...",
-//                            Snackbar.LENGTH_LONG).show();
-//                    mediaPlayer.start();
-//                    mediaPlayer.setLooping(true);
-//                } else {
-//                    Snackbar.make(rainBtn, "Gentle rain stopped...",
-//                            Snackbar.LENGTH_LONG).show();
-//                    mediaPlayer.pause();
-//                }
-//            }
-//        });
-
         rainBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LoopMediaPlayer.create(MainActivity.this, R.raw.raining_sound);
+                mResId = R.raw.raining_sound;
+                setCurrentPlayer();
             }
         });
+    }
+
+    private void setCurrentPlayer() {
+        if (!mCurrentPlayer.isPlaying()) {
+            mCurrentPlayer = MediaPlayer.create(MainActivity.this, mResId);
+            mCurrentPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mCurrentPlayer.start();
+                }
+            });
+        } else {
+            mCurrentPlayer.pause();
+        }
+    }
+
+    private void createNextMediaPlayer() {
+        mNextPlayer = MediaPlayer.create(this, mResId);
+        mCurrentPlayer.setNextMediaPlayer(mNextPlayer);
+        mCurrentPlayer.setOnCompletionListener(onCompletionListener);
     }
 }
